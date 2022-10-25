890d9789000e54b152c606fa28ca98929947189d
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-25 11:18:26 +0300

    completed phase 3 metaprogramming custom errors

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,14 +6,31 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error # The rescue method creates an instance of the PartnerError class and puts out the result of calling message on that instance
+        puts error.message  
+      end
+    end
+  end
+
+  class PartnerError < StandardError # Define the custom error
+
+    def message
+      "you must give the get_married method an argument of an instance of the person class" # writing a Custom Error Message
+    end
+
   end
 
 end
 
 beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
+jay = Person.new("Z")
+beyonce.get_married(jay)
 puts beyonce.name
 
 
